f9e8ea1b456abfe7dd4ca96f3cc1aaaf83cdc8bf
Alex Clemmer <clemmer.alexander@gmail.com>
2013-05-05 04:29:25 -0600

    Add main to run lexer

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,5 @@
-import Test.HUnit
+import System.IO (hGetContents, stdin)
 import qualified Lexer as Lexer
 
-main :: IO Counts
-main = Lexer.main
+main :: IO ()
+main = (hGetContents stdin) >>= Lexer.emit . Lexer.lex
